ec8f24b3dd7a679f5c5f492e0a89cdd946837f34
YoonHo Kang <ace@pnpeople.com>
2020-03-19 08:23:59 +0900

    GenieMobileBoilerplate로 프로젝트 이름 변경

--- android/app/src/main/java/services/genii/geniemobileboilerplate/MainActivity.java
+++ android/app/src/main/java/services/genii/geniemobileboilerplate/MainActivity.java
@@ -1,6 +1,6 @@
 //*.for react-native 0.61.5, checked at 2020-02-17
 
-package com.reactnativeapp;
+package services.genii.geniemobileboilerplate;
 
 import com.facebook.react.ReactActivity;
 //*.react-native-orientation-locker
@@ -9,14 +9,14 @@ import android.content.res.Configuration;
 
 public class MainActivity extends ReactActivity {
 
-  /**
-   * Returns the name of the main component registered from JavaScript. This is used to schedule
-   * rendering of the component.
-   */
-  @Override
-  protected String getMainComponentName() {
-    return "ReactNativeApp";
-  }
+	/**
+	* Returns the name of the main component registered from JavaScript. This is used to schedule
+	* rendering of the component.
+	*/
+	@Override
+	protected String getMainComponentName() {
+		return "GenieMobileBoilerplate";
+	}
 
 	//*.react-native-orientation-locker
 	@Override
--- ios/GenieMobileBoilerplateTests/GenieMobileBoilerplateTests.m
+++ ios/GenieMobileBoilerplateTests/GenieMobileBoilerplateTests.m
@@ -7,11 +7,11 @@
 #define TIMEOUT_SECONDS 600
 #define TEXT_TO_LOOK_FOR @"Welcome to React"
 
-@interface ReactNativeAppTests : XCTestCase
+@interface GenieMobileBoilerplateTests : XCTestCase
 
 @end
 
-@implementation ReactNativeAppTests
+@implementation GenieMobileBoilerplateTests
 
 - (BOOL)findSubviewInView:(UIView *)view matching:(BOOL(^)(UIView *view))test
 {
