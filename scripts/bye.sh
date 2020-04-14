d71d5be3c39034fbd06de1f234741a10bbeb3a41
강윤호 <ace@ace-imac-nets.local>
2020-04-14 19:21:18 +0900

    Hi & Bye 스크립트

---
+++ scripts/bye.sh
@@ -0,0 +1,17 @@
+#!/usr/bin/env bash
+########################################################################################
+##
+##	'The end of this work' script for UN*X
+##
+##  작성자: YoonHo Kang appcreatier@gmail.com
+##  V1.0 2020-04-14
+##
+##	사용법:
+##	yarn bye
+##
+########################################################################################
+
+git push origin master:master
+echo 10초 후 맥을 재웁니다.
+sleep 10
+pmset displaysleepnow
