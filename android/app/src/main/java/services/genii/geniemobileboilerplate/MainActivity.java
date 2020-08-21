ec2919a15cf65b4b5453a259cfc94bcb0291de75
YoonHo Kang <appcreatier@gmail.com>
2020-08-21 13:38:30 +0900

    react native 0.63.2 대응

--- android/app/src/main/java/services/genii/geniemobileboilerplate/MainActivity.java
+++ android/app/src/main/java/services/genii/geniemobileboilerplate/MainActivity.java
@@ -1,14 +1,10 @@
+//*.for react-native 0.63.2, checked at 2020-08-21
 //*.for react-native 0.61.5, checked at 2020-02-17
 
 package services.genii.geniemobileboilerplate;
 
-import android.app.Activity;
-
-import android.os.Bundle;
-import androidx.annotation.Nullable;
-
 import com.facebook.react.ReactActivity;
-import com.facebook.react.ReactActivityDelegate;
+
 //*.react-native-orientation-locker
 import android.content.Intent;
 import android.content.res.Configuration;
@@ -16,15 +12,15 @@ import android.content.res.Configuration;
 public class MainActivity extends ReactActivity {
 
 	/**
-	* Returns the name of the main component registered from JavaScript. This is used to schedule
-	* rendering of the component.
-	*/
+	 * Returns the name of the main component registered from JavaScript. This is used to schedule
+	 * rendering of the component.
+	 */
 	@Override
 	protected String getMainComponentName() {
-		return "GenieMobileBoilerplate";
+	return "GenieMobileBoilerplate";
 	}
 
-	//*.react-native-orientation-locker
+  	//*.react-native-orientation-locker
 	@Override
 	public void onConfigurationChanged(Configuration newConfig) {
 		super.onConfigurationChanged(newConfig);
@@ -33,34 +29,6 @@ public class MainActivity extends ReactActivity {
 		this.sendBroadcast(intent);
 	}
 
-	public static class AuthActivityDelegate extends ReactActivityDelegate{
-		private Bundle mInitialProps = null;
-        private final @Nullable Activity mActivity;
-		public AuthActivityDelegate(Activity activity, String mainComponentName) {
-            super(activity, mainComponentName);
-            this.mActivity = activity;
-        }
-
-		@Override
-        protected void onCreate(Bundle savedInstanceState) {
-			Bundle bundle = mActivity.getIntent().getExtras();
-			if( bundle != null && bundle.containsKey("packageName") && bundle.containsKey("appId")){
-				mInitialProps = new Bundle();
-				mInitialProps.putString("packageName", bundle.getString("packageName"));
-				mInitialProps.putString("appId", bundle.getString("appId"));
-			}
-			super.onCreate(savedInstanceState);
-		}
-
-		@Override
-        protected Bundle getLaunchOptions() {
-            return mInitialProps;
-        }
-	};
-
-	@Override
-    protected ReactActivityDelegate createReactActivityDelegate() {
-        return new AuthActivityDelegate(this, getMainComponentName());
-    }
+	//*.ReactActivityDelegate 관련 모듈 제거됨
 
 }
